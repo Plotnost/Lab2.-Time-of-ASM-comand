86778fe71b7bc982a78e6311b12ab44ba1886129
Роман Сингур <zoo1102@mail.ru>
2023-12-01 18:53:13 +0500

    Попытка 1

--- Lab2. Time of ASM-comand.cpp
+++ Lab2. Time of ASM-comand.cpp
@@ -1,20 +1,69 @@
 ﻿// Lab2. Time of ASM-comand.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
-#include <iostream>
+#include <windows.h>
+#include <stdio.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    __int64 clock1 = 0xaabbccdd112233ff;
+    __int64 clock2 = 0xaabbccdd112233ff;
+    unsigned long long ticks1;
+    unsigned long long ticks2;
+    unsigned long long tick_of_comand;
+
+    for (int i = 0; i <= 5; i++) {
+        __asm
+        {
+            rdtsc; edx:eax
+            mov dword ptr[clock1], eax
+            mov dword ptr[clock1 + 4], edx
+
+            mov ecx, 0x1fffffff
+            m1:
+            mov eax, 3
+            add eax, 2
+            mov eax, 3
+            add eax, 2
+            mov eax, 3
+            add eax, 2
+            mov eax, 3
+            add eax, 2
+            loop m1
+
+            rdtsc; edx:eax
+            mov dword ptr[clock2], eax
+            mov dword ptr[clock2 + 4], edx
+
+        }
+
+        ticks1 = clock2 - clock1;
+
+        clock1 = 0xaabbccdd112233ff;
+        clock2 = 0xaabbccdd112233ff;
+
+        __asm
+        {
+            rdtsc; edx:eax
+            mov dword ptr[clock1], eax
+            mov dword ptr[clock1 + 4], edx
+
+            mov ecx, 0x1fffffff
+            m2:
+            loop m2
+
+            rdtsc; edx:eax
+            mov dword ptr[clock2], eax
+            mov dword ptr[clock2 + 4], edx
+
+        }
+
+        ticks2 = clock2 - clock1;
+        tick_of_comand = ticks1 - ticks2;
+
+        printf("%lld\n%lld\n", ticks1, ticks2);
+        printf("%f\n%lld\n", float(tick_of_comand / 536870911), tick_of_comand);
+    }
+    return 0;
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
