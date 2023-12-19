2093544770a39e8779f95cf72adf61dbc323ea89
Plotnost <zoo1102@mail.ru>
2023-12-19 17:38:35 +0500

    Prafki

--- Lab2. Time of ASM-comand.cpp
+++ Lab2. Time of ASM-comand.cpp
@@ -1,69 +1,67 @@
 ﻿// Lab2. Time of ASM-comand.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
+#include <iostream>
 #include <windows.h>
-#include <stdio.h>
+
+    using namespace std;
 
 int main()
 {
-    __int64 clock1 = 0xaabbccdd112233ff;
-    __int64 clock2 = 0xaabbccdd112233ff;
-    unsigned long long ticks1;
-    unsigned long long ticks2;
-    unsigned long long tick_of_comand;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-    for (int i = 0; i <= 5; i++) {
-        __asm
-        {
-            rdtsc; edx:eax
-            mov dword ptr[clock1], eax
-            mov dword ptr[clock1 + 4], edx
+    __int64 start, end, elapsed;
 
-            mov ecx, 0x1fffffff
-            m1:
-            mov eax, 3
-            add eax, 2
-            mov eax, 3
-            add eax, 2
-            mov eax, 3
-            add eax, 2
-            mov eax, 3
-            add eax, 2
+    for (int i = 0; i < 10; i++) {
+        // Получаем текущее время
+        __asm {
+            rdtsc;          // Чтение временной метки процессора
+            mov dword ptr[start], eax
+                mov dword ptr[start + 4], edx // Сохраняем начальное время
+
+                mov ecx, 0x11111111
+                m1:
+            MOV EAX, 1000; // Делимое
+            MOV EDX, 0;    // Обнуляем остаток
+            MOV EBX, 1;    // Делитель
+            DIV EBX;       // EAX = EAX / EBX
+
             loop m1
 
-            rdtsc; edx:eax
-            mov dword ptr[clock2], eax
-            mov dword ptr[clock2 + 4], edx
-
+                rdtsc;          // Чтение временной метки процессора
+            mov dword ptr[end], eax
+                mov dword ptr[end + 4], edx   // Сохраняем конечное время
         }
 
-        ticks1 = clock2 - clock1;
+        // Рассчитываем разницу во времени
+        elapsed = end - start;
 
-        clock1 = 0xaabbccdd112233ff;
-        clock2 = 0xaabbccdd112233ff;
+        // Получаем текущее время
+        __asm {
+            rdtsc;          // Чтение временной метки процессора
+            mov dword ptr[start], eax
+                mov dword ptr[start + 4], edx // Сохраняем начальное время
 
-        __asm
-        {
-            rdtsc; edx:eax
-            mov dword ptr[clock1], eax
-            mov dword ptr[clock1 + 4], edx
-
-            mov ecx, 0x1fffffff
-            m2:
+                mov ecx, 0x11111111
+                m2:
+            MOV EAX, 1000; // Делимое
+            MOV EDX, 0;    // Обнуляем остаток
+            MOV EBX, 1;    // Делитель
             loop m2
 
-            rdtsc; edx:eax
-            mov dword ptr[clock2], eax
-            mov dword ptr[clock2 + 4], edx
-
+                rdtsc;          // Чтение временной метки процессораы
+            mov dword ptr[end], eax
+                mov dword ptr[end + 4], edx   // Сохраняем конечное время
         }
 
-        ticks2 = clock2 - clock1;
-        tick_of_comand = ticks1 - ticks2;
+        // Рассчитываем разницу во времени
+        elapsed = elapsed - (end - start);
 
-        printf("%lld\n%lld\n", ticks1, ticks2);
-        printf("%f\n%lld\n", float(tick_of_comand / 536870911), tick_of_comand);
+        std::cout << "Время выполнения команды в " << i + 1 << " итерации: " << elapsed / 286331153 << " тактов" << endl;
     }
-    return 0;
 
+    system("pause");
+
+    return 0;
 }
